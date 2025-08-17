27774fba5437c93b2ffe6fcdd01d030fb677b445
Hamza <hmza.rmh@gmail.com>
2025-08-17 15:54:23 +0300

    Update test.py

--- Module/Illusion/test.py
+++ Module/Illusion/test.py
@@ -1,41 +1,29 @@
-from script import v as v
-from random import shuffle
-from random import randint as ri
-import os
+from script import *
+from string import ascii_lowercase as acl, ascii_uppercase as acu, digits as dg
 
-cl = v.const_list
-cs = v.const_string
-INfile = b'             <E>        \n<6>'
-var = INfile.decode().split()
-for e in var: print(e[1:-1])
-
-def zReplace(x, l1, l2):
-    for s, y in zip(l1, l2): x.replace(s, y)
-    return x
-
-def getkey(file):
-    lst = []
-    if file:
-        with open(file, 'rb') as f:
-            content = f.read().decode().split()
-        for e in content: lst.append(e[1:-1])
-    return lst
-
-def genkey(el, file=False):
-    shuffle(el)
-    res, ext = ''.join(c for c in el), '.txt'
-    if file:
-        format = f'{res}' + ext
-        if os.path.exists(format):
-            for i in range(1000):
-                FORMAT, format = f'{res}' + str(i) + ext, ''
-                if not os.path.exists(FORMAT): break
-        fFormat = FORMAT if not format else format
-        with open(fFormat, 'w') as f:
-            for c in el:
-                f.write(f"{ri(0, 99)*' '}<{c}>\n")
-    return res
+Alphabet = acl+acu+dg+'+/'
+key = getkey('key.key')
+print(key)
+print()
+print(v.const_list)
+print()
+for x, y in zip(key, v.const_list):
+    print(f'{x} : {y}')
+print()
 
 
-print(genkey(list(v.const_string), True))
-print(getkey('jsOKIUvkruoaPSwfimJyFdpGLnqA.txt'))
+print(Alphabet)
+print()
+
+mybin = turn_binary(Alphabet, 'to')
+print(mybin)
+print()
+compressed = comp(mybin)
+print(compressed)
+print()
+bh = enc(compressed, key)
+print(bh)
+
+o = turn_binary(decomp(dec(bh, key)), 'from')
+print()
+print(o)
